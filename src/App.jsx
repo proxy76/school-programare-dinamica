15c815faf7b7769b8de9144d47a8c8d20b462b34
Radulescu Razvan <proxy.razvan@gmail.com>
2023-12-23 14:37:15 +0200

    add: background effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header'
+import MainTitle from './components/MainTitle';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  document.body.addEventListener("pointermove", (e)=>{
+    const { currentTarget: el, clientX: x, clientY: y } = e;
+    const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+    el.style.setProperty('--posX',  x-l-w/2);
+    el.style.setProperty('--posY',  y-t-h/2);
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Header />
+     <MainTitle />
     </>
   )
 }
---
+++ src/components/MainTitle.jsx
@@ -0,0 +1,28 @@
+import './MainTitle.scss'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useRef } from 'react'
+
+export default function MainTitle() {
+    const ref = useRef(null)
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start center", "end start"],
+    })
+
+    const opacity = useTransform(scrollYProgress, [0, 1], [1, 0])
+    const scale = useTransform(scrollYProgress, [0, 0.5], [1, 0.8])
+
+    return (
+        <>
+            <div className="main">
+                <motion.p
+                ref={ref}
+                style={{scale, opacity}}
+                >
+                    Programare dinamică
+                </motion.p>
+                
+            </div>
+        </>
+    )
+}
